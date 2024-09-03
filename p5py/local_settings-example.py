53d578c3dbf937cbe170108b6364ce623655e9f4
Anderson <andersonby@163.com>
2024-09-03 11:51:02 +0800

    Update Django project for version 5.1 compatibility and cleanup.

---
+++ p5py/local_settings-example.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2%!%2me*vs$u)848urlx!vaq1tn9_qr=m$e)dmu^3k!k9s$grd"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1"]
